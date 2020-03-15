f83060db9b6aeefd72783d2c74d12b4f76852122
DaiGuard <zenith.or.w@gmail.com>
2020-03-15 18:24:47 +0900

    Updates

---
+++ precache-manifest.3866c15261b3572d2bc3b8c1280a00ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd8e0ca38de57e871586117fc4e275c",
+    "url": "/github-pages-test/index.html"
+  },
+  {
+    "revision": "46f9496f9a1288fbfed2",
+    "url": "/github-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3617a4fc5e569322d0de",
+    "url": "/github-pages-test/static/js/2.64107019.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github-pages-test/static/js/2.64107019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f9496f9a1288fbfed2",
+    "url": "/github-pages-test/static/js/main.39d6f8f8.chunk.js"
+  },
+  {
+    "revision": "2a37991c5a1315087e4d",
+    "url": "/github-pages-test/static/js/runtime-main.9ff48b04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
